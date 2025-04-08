04da67bea9c5621e5802252072cc20e8ef170b20
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-04-08 16:02:36 -0400

    Navbar made responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,17 +10,18 @@ import {
   ListItem,
   ListItemText,
   useMediaQuery,
+  Box,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
+import { useTheme } from "@mui/material/styles";
 import styles from "./Navbar.module.css";
 import logo from "../../assets/logo.png";
-import { useTheme } from "@mui/material/styles";
 
 const Navbar = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   const toggleDrawer = () => setDrawerOpen(!drawerOpen);
 
@@ -31,45 +32,43 @@ const Navbar = () => {
   ];
 
   return (
-    <>
-      <AppBar position="sticky" className={styles.appBar}>
-        <Toolbar className={styles.toolbar}>
-          <Typography
-            variant="h6"
-            className={styles.navbarTitle}
-            style={{ flexGrow: 1 }}
-          >
-            <img src={logo} alt="Logo" className={styles.logo} />
+    <AppBar position="sticky" className={styles.appBar}>
+      <Toolbar className={styles.toolbar}>
+        <Box display="flex" alignItems="center" className={styles.logoWrapper}>
+          <img src={logo} alt="Logo" className={styles.logo} />
+          <Typography variant="h6" className={styles.navbarTitle}>
             Pepper
           </Typography>
+        </Box>
 
-          {isMobile ? (
-            <>
-              <IconButton edge="end" color="inherit" onClick={toggleDrawer}>
-                <MenuIcon />
-              </IconButton>
-              <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer}>
-                <List className={styles.drawerList}>
-                  {navItems.map((item) => (
-                    <ListItem
-                      button
-                      key={item.to}
-                      className={styles.drawerListItem}
-                      onClick={toggleDrawer}
-                    >
-                      <Link to={item.to} className={styles.drawerLink}>
-                        <ListItemText
-                          primary={item.label}
-                          className={styles.drawerItemText}
-                        />
-                      </Link>
-                    </ListItem>
-                  ))}
-                </List>
-              </Drawer>
-            </>
-          ) : (
-            navItems.map((item) => (
+        {isMobile ? (
+          <>
+            <IconButton edge="end" color="inherit" onClick={toggleDrawer}>
+              <MenuIcon />
+            </IconButton>
+            <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer}>
+              <List className={styles.drawerList}>
+                {navItems.map((item) => (
+                  <ListItem
+                    button
+                    key={item.to}
+                    className={styles.drawerListItem}
+                    onClick={toggleDrawer}
+                  >
+                    <Link to={item.to} className={styles.drawerLink}>
+                      <ListItemText
+                        primary={item.label}
+                        className={styles.drawerItemText}
+                      />
+                    </Link>
+                  </ListItem>
+                ))}
+              </List>
+            </Drawer>
+          </>
+        ) : (
+          <Box display="flex">
+            {navItems.map((item) => (
               <Button
                 key={item.to}
                 color="inherit"
@@ -79,11 +78,11 @@ const Navbar = () => {
               >
                 {item.label}
               </Button>
-            ))
-          )}
-        </Toolbar>
-      </AppBar>
-    </>
+            ))}
+          </Box>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 };
 
