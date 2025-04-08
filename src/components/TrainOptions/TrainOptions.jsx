6488105e776ebf819d8a0ab455170a14e69cfe04
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-04-07 22:31:31 -0400

    Animations added to home page

--- src/components/TrainOptions/TrainOptions.jsx
+++ src/components/TrainOptions/TrainOptions.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Grid, Button, Box } from '@mui/material';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Grid, Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const TrainOptions = () => {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 4 }}>
+    <Box sx={{ display: "flex", justifyContent: "center", marginTop: 4 }}>
       <Grid container spacing={3} justifyContent="center" alignItems="center">
         <Grid item xs={12} sm={6} md={4}>
           <Button
@@ -14,6 +14,13 @@ const TrainOptions = () => {
             component={Link}
             to="/train/file"
             size="large"
+            sx={{
+              transition: "transform 0.2s ease",
+              "&:hover": {
+                transform: "scale(1.03)",
+                boxShadow: 4,
+              },
+            }}
           >
             Upload CSV/JSON File to Train
           </Button>
@@ -26,6 +33,13 @@ const TrainOptions = () => {
             component={Link}
             to="/train/manual"
             size="large"
+            sx={{
+              transition: "transform 0.2s ease",
+              "&:hover": {
+                transform: "scale(1.03)",
+                boxShadow: 4,
+              },
+            }}
           >
             Enter Manual Input to Train
           </Button>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,13 +3,20 @@ import Navbar from "../../components/Navbar/Navbar";
 import TitleTagline from "../../components/TitleTagline/TitleTagline";
 import Footer from "../../components/Footer/Footer";
 import TrainOptions from "../../components/TrainOptions/TrainOptions";
+import FeatureHighlights from "../../components/FeatureHighlights/FeatureHighlights";
+import HowItWorks from "../../components/HowItWorks/HowItWorks";
+import CallToAction from "../../components/CallToAction/CallToAction";
+import styles from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <Navbar />
       <TitleTagline />
       <TrainOptions />
+      <FeatureHighlights />
+      <HowItWorks />
+      <CallToAction />
       <Footer />
     </div>
   );
