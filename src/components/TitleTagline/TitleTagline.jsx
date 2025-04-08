4eaf67df668617da874595e44b8804ae1c6abab5
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-04-08 11:22:48 -0400

    Tagline css

--- src/components/TitleTagline/TitleTagline.jsx
+++ src/components/TitleTagline/TitleTagline.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Typography, Container, Box } from '@mui/material';
+import styles from './TitleTagline.module.css';
 
 const TitleTagline = () => {
   return (
-    <Container>
-      <Box textAlign="center" sx={{ marginBottom: 4 }}>
-        <Typography variant="h4" gutterBottom>
+    <Container className={styles.container}>
+      <Box className={styles.box}>
+        <Typography variant="h4" gutterBottom className={styles.heading}>
           Smart Sales Forecasting and Reorder Recommendations
         </Typography>
-        <Typography variant="h6" color="textSecondary">
+        <Typography variant="h6" className={styles.subheading}>
           Choose how you want to train the model and make predictions
         </Typography>
       </Box>
