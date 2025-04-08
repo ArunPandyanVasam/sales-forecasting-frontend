9546f02a553c2d3485a76ad23b325a643fe9823c
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-04-07 21:01:59 -0400

    Routing between pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import Router from "../Router"
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import TrainPage from "./pages/TrainPage/TrainPage";
+import PredictPage from "./pages/PredictPage/PredictPage";
 
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/train" element={<TrainPage />} />
+        <Route path="/predict" element={<PredictPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
-function App() {
-
-  return <Router />
-}
-
-export default App
+export default App;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import TitleTagline from "../../components/TitleTagline/TitleTagline";
+import Footer from "../../components/Footer/Footer";
+import { useNavigate } from "react-router-dom";
+import TrainOptions from "../../components/TrainOptions/TrainOptions";
 
 const HomePage = () => {
+  const navigate = useNavigate();  // Hook for navigation
+
+  const handleTrainingOption = (option) => {
+    if (option === 'file') {
+      navigate('/train');  // Navigate to TrainPage (with file upload option)
+    } else if (option === 'manual') {
+      navigate('/train');  // Navigate to TrainPage (with manual input option)
+    }
+  };
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <Navbar />
+      <TitleTagline />
+      <TrainOptions onSelectOption={handleTrainingOption} />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
