2995a53e7b0b72b5aaba2ccf2af7874db37a275f
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-04-08 11:53:07 -0400

    Animation applied

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Box, Typography, Container } from '@mui/material';
+import styles from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <Box sx={{ backgroundColor: '#1976d2', color: 'white', padding: 2 }}>
+    <Box className={styles.footer}>
       <Container>
-        <Typography variant="body1" align="center">
+        <Typography variant="body1" align="center" className={styles.footerText}>
           &copy; {new Date().getFullYear()} Smart Sales Forecasting. All Rights Reserved.
         </Typography>
       </Container>
