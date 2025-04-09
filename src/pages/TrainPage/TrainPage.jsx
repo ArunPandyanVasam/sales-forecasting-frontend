2b4941c3ddae3f661f448567b2b6f4800e9b813e
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-04-09 12:47:24 -0400

    Navbar and Footer add to Train Page along with FileUpload Component

--- src/pages/TrainPage/TrainPage.jsx
+++ src/pages/TrainPage/TrainPage.jsx
@@ -1,25 +1,48 @@
-import React, { useState } from 'react';
-import FileUploadForm from '../../components/FileUploadForm/FileUploadForm';
-import { Container, Box, Typography } from '@mui/material';
+import React, { useState } from "react";
+import FileUploadForm from "../../components/FileUploadForm/FileUploadForm";
+import { Container, Box, Typography } from "@mui/material";
+import styles from "./TrainPage.module.css";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import CallToAction from "../../components/CallToAction/CallToAction";
 
 const TrainPage = () => {
   const [modelTrained, setModelTrained] = useState(false);
 
   return (
-    <Container>
-      <Box mt={4}>
-        <Typography variant="h4" align="center">Train Model with File</Typography>
-        <Typography variant="body1" align="center" gutterBottom>
-          Upload your CSV/JSON file to train the model.
-        </Typography>
-        
-        {!modelTrained ? (
-          <FileUploadForm onModelTrained={() => setModelTrained(true)} />
-        ) : (
-          <Typography variant="h6" align="center">Model trained successfully! Now you can make predictions.</Typography>
-        )}
-      </Box>
-    </Container>
+    <div>
+      <Navbar />
+      <Container className={styles.trainContainer}>
+        <Box className={styles.trainBox}>
+          <Typography variant="h4" align="center" className={styles.title}>
+            Train Model with File
+          </Typography>
+          <Typography
+            variant="body1"
+            align="center"
+            className={styles.subtitle}
+          >
+            Upload your CSV/JSON file to train the model.
+          </Typography>
+
+          {!modelTrained ? (
+            <div className={styles.uploadFormWrapper}>
+              <FileUploadForm onModelTrained={() => setModelTrained(true)} />
+            </div>
+          ) : (
+            <Typography
+              variant="h6"
+              align="center"
+              className={styles.successMessage}
+            >
+              Model trained successfully! Now you can make predictions.
+            </Typography>
+          )}
+        </Box>
+      </Container>
+      <CallToAction />
+      <Footer />
+    </div>
   );
 };
 
