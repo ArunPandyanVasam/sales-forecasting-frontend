0f7ff8fb3efeec506eaab32507e6c887f36caf4c
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-04-15 12:31:45 -0400

    hero image

--- src/pages/TrainPage/TrainPage.jsx
+++ src/pages/TrainPage/TrainPage.jsx
@@ -5,11 +5,11 @@ import styles from "./TrainPage.module.css";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import CallToAction from "../../components/CallToAction/CallToAction";
-
 import BenefitsOfTraining from "../../components/BenefitsOfTraining/BenefitsOfTraining";
 import FAQBox from "../../components/FAQBox/FAQBox";
 import TrainingInstructions from "../../components/TrainingInstructions/TrainingInstructions";
 import PredictionForm from "../../components/PredictionForm/PredictionForm";
+import salesGraphImage from "../../assets/images/salesGrapgh.jpg";
 
 const TrainPage = () => {
   const [modelTrained, setModelTrained] = useState(false);
@@ -26,29 +26,21 @@ const TrainPage = () => {
 
       <div className={styles.mainContent}>
         <Grid container spacing={4} justifyContent="space-evenly">
-          {/* Benefits Section */}
           <Grid item xs={12} md={6} className={styles.gridItem}>
             <BenefitsOfTraining />
           </Grid>
-
-          {/* Training Instructions Section */}
           <Grid item xs={12} md={6} className={styles.gridItem}>
             <TrainingInstructions />
           </Grid>
         </Grid>
       </div>
 
-      {/* File Upload Section */}
       <div className={styles.trainContainer}>
         <Box className={styles.trainBox}>
           <Typography variant="h4" align="center" className={styles.title}>
             Train Model with File
           </Typography>
-          <Typography
-            variant="body1"
-            align="center"
-            className={styles.subtitle}
-          >
+          <Typography variant="body1" align="center" className={styles.subtitle}>
             Upload your CSV/JSON file to train the model.
           </Typography>
 
@@ -68,9 +60,26 @@ const TrainPage = () => {
         </Box>
       </div>
 
-      <PredictionForm />
+      <div className={styles.predictionSection}>
+        <Grid container spacing={4} alignItems="center" justifyContent="center">
+          <Grid item xs={12} md={6} className={styles.predictionFormWrapper}>
+            <PredictionForm />
+          </Grid>
+          <Grid item xs={12} md={6} className={styles.predictionImageWrapper}>
+            <div className={styles.imageContainer}>
+              <Typography variant="h5" align="center" className={styles.imageTitle}>
+                Forecasting Sales Trends
+              </Typography>
+              <img
+                src={salesGraphImage}
+                alt="Sales Forecast Graph"
+                className={styles.predictionImage}
+              />
+            </div>
+          </Grid>
+        </Grid>
+      </div>
 
-      {/* FAQ Section */}
       <div className={styles.faqSection}>
         <FAQBox />
       </div>
